aa3cb1607c568d5ef0f76d0d07ff15caef3b72bc
Vidar Magnusson <git@vidarmagnusson.com>
2026-07-24 11:46:51 +0200

    Fix doc comment style

--- storage/storage-common/src/storage.rs
+++ storage/storage-common/src/storage.rs
@@ -13,68 +13,54 @@ pub enum StorageError {
 pub type StorageResult<T> = Result<T, StorageError>;
 
 pub trait Storage: Send + Sync {
-    /**
-     * Save our own identity to storage, only one identity can ever be stored.
-     */
+    /// Save our own identity to storage, only one identity can ever be stored.
     fn save_identity(&self, id: &keystone::Identity) -> StorageResult<()>;
-    /**
-     * Load our own identity from storage, returns None if no identity is saved.
-     */
+
+    /// Load our own identity from storage, returns None if no identity is saved.
     fn load_identity(&self) -> StorageResult<Option<keystone::Identity>>;
-    /**
-     * Save a friend to storage.
-     */
+
+    /// Save a friend to storage.
     fn save_friend(&self, f: &keystone::Friend) -> StorageResult<()>;
-    /**
-     * Load all friends from storage.
-     */
+
+    /// Load all friends from storage.
     fn load_friends(&self) -> StorageResult<Vec<keystone::Friend>>;
-    /**
-     * Load a specific friend by their public signing key, returns None if no friend is found.
-     */
+
+    /// Load a specific friend by their public signing key, returns None if no friend is found.
     fn load_friend_by_sign_pub(
         &self,
         sign_pub: &SigningPublicKey,
     ) -> StorageResult<Option<keystone::Friend>>;
-    /**
-     * Save a profile to storage.
-     */
+
+    /// Save a profile to storage.
     fn save_profile(&self, p: &keystone::Profile) -> StorageResult<()>;
-    /**
-     * Load a profile from storage by the owner's public signing key, returns None if no profile is found.
-     */
+
+    /// Load a profile from storage by the owner's public signing key, returns None if no profile is found.
     fn load_profile(
         &self,
         owner_sign_pub: &SigningPublicKey,
     ) -> StorageResult<Option<keystone::Profile>>;
-    /**
-     * Save a post to storage, returns true if the post was "new" and had not been saved before.
-     */
+
+    /// Save a post to storage, returns true if the post was "new" and had not been saved before.
     fn save_post(&self, post: &keystone::Post, body: &str) -> StorageResult<bool>;
-    /**
-     * Load all posts from storage, returns an empty vector if no posts are found.
-     */
+
+    /// Load all posts from storage, returns an empty vector if no posts are found.
     fn load_posts(&self) -> StorageResult<Vec<StoredPost>>;
-    /**
-     * Save a response to storage, returns true if the response was "new" and had not been saved before.
-     */
+
+    /// Save a response to storage, returns true if the response was "new" and had not been saved before.
     fn save_response(&self, post_id: &PostId, response: &StoredResponse) -> StorageResult<bool>;
-    /**
-     * Load all responses for a specific post from storage, returns an empty vector if no responses are found.
-     */
+
+    /// Load all responses for a specific post from storage, returns an empty vector if no responses are found.
     fn load_responses_for(&self, post_id: &PostId) -> StorageResult<Vec<StoredResponse>>;
-    /**
-     * Get the last index of a post for a specific friend, direction and epoch. Returns the last index.
-     */
+
+    /// Get the last index of a post for a specific friend, direction and epoch. Returns the last index.
     fn get_cursor(
         &self,
         friend_sign_pub: &SigningPublicKey,
         direction: u8,
         epoch: u64,
     ) -> StorageResult<usize>;
-    /**
-     * Set the last index of a post for a specific friend, direction and epoch.
-     */
+
+    /// Set the last index of a post for a specific friend, direction and epoch.
     fn set_cursor(
         &self,
         friend_sign_pub: &SigningPublicKey,
